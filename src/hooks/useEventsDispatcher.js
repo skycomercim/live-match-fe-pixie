feb1b04e706cdbe527df68922a7af4e586c5ddc6
GARELLA Roberto <roberto.garella@skytv.it>
2023-03-24 12:04:59 +0100

    new hook

---
+++ src/hooks/useEventsDispatcher.js
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react"
+
+const useEventsDispatcher = () => {
+
+    const eventsRef = useRef([]);
+    const [event, setEvent] = useState();
+    const lastEventDate = useRef(new Date());
+    const timeoutId = useRef();
+
+    const eventDispatcher = () => {
+        const events = eventsRef.current;
+
+        if (events.length > 0) {
+            const nextEvent = eventsRef.current.pop();
+            const { timestamp_utc } = nextEvent;
+            const nextEventDate = new Date(timestamp_utc);
+            timeoutId.current = setTimeout(() => {
+                setEvent(nextEvent);
+                lastEventDate.current = nextEventDate;
+                eventDispatcher();
+            }, (nextEventDate.getTime() - lastEventDate.getTime()))
+        }
+        else {
+            timeoutId.current = setTimeout(() => {
+                eventDispatcher();
+            }, 100);
+        }
+    }
+
+    useEffect(() => {
+        eventDispatcher();
+    }, [])
+
+    return {
+        event,
+        addEvents: (events) => eventsRef.current.push(...events)
+    }
+};
+
+export default useEventsDispatcher;
